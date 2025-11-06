346ee61543f3f3797449b2e31684675d0eb3a48b
Jothika12377 <jothikanerella67@gmail.com>
2025-11-06 09:55:07 +0530

    Create amstrongnumber.c

---
+++ amstrongnumber.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,s=0,r,t,dig=0;
+	printf("Enter n value:");
+	scanf("%d",&n);
+	t=n;
+	while(t>0)
+	{
+		r=t%10;
+		dig=dig+1;
+		t=t/10;
+	}
+	//Logic for finding sum of powers of digits
+	t=n;
+	while(t>0)
+	{
+		r=t%10;
+		s=s+pow(r,dig);
+		t=t/10;
+	}
+	if(n==s)//checking original is same or not
+	printf("%d is ARMSTRONG NUMBER",n);
+	else
+	printf("%d is NOT A ARMSTRONG NUMBER",n);
+	return 0;
+}
