fbde7826023d2ff3b8126ea6b04138baa7382887
Jothika12377 <jothikanerella67@gmail.com>
2025-11-06 09:54:24 +0530

    Create simpleinterest.c

---
+++ simpleinterest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int p,t,r,si;
+	printf("value of p");
+	scanf("%d",&p);
+	printf("value of t");
+	scanf("%d",&t);
+	printf("value of r");
+	scanf("%d",&r);
+	si= (p*t*r)/100.0;
+	printf("\n SI=%f",si);
+	return 0;
+
+}
